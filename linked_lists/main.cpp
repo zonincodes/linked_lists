66d6918ba1a99d86eeb4c64ac0406af2cad0a292
Zonin <business.kariukimartin@gmail.com>
2023-09-25 21:05:07 +0300

    Add project files.

---
+++ linked_lists/main.cpp
@@ -0,0 +1,15 @@
+// this is file contains the entry
+
+// include files
+#include <iostream>
+#include <string>
+
+
+// namespaces
+using namespace std;
+
+// main function - program entry point
+int main()
+{
+	cout << "Hello, World!" << endl;
+}
